6dbf513948152f36d47a3fa3f530c7016bfd0d68
erna-h33 <erna.halim33@gmail.com>
2025-05-31 19:44:43 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Button from "./Button.jsx";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click Me</Button>
+      </div>
+      <div>
+        <Button>Buy Now</Button>
+      </div>
+      <div>
+        <Button>Subscribe</Button>
+      </div>
+      <div>
+        <Button>Read More</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
