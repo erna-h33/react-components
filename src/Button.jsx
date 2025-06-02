4baadcb09b396c51d3949a10c714d78199ee1e84
erna-h33 <erna.halim33@gmail.com>
2025-06-02 17:58:09 +1000

    Done for button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -10,9 +10,10 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
   const classes = twMerge(
-    className("px-3 py-1.5 border flex items-center", {
+    className(rest.className, "px-3 py-1.5 border flex items-center", {
       "border-blue-500 bg-blue-500 text-white": primary,
       "border-gray-900 bg-gray-900 text-white": secondary,
       "border-green-500 bg-green-500 text-white": success,
@@ -28,7 +29,11 @@ function Button({
     })
   );
 
-  return <button className={classes}>{children}</button>;
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
