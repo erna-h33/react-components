cb115df0d267ce923e3c6173c675257e602f1e4d
erna-h33 <erna.halim33@gmail.com>
2025-06-19 09:55:15 +1000

    Modal completed

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -9,12 +9,29 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <Button onClick={handleClose} primary>
+      {" "}
+      I Accept
+    </Button>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
   return (
-    <div>
+    <div className="relative">
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
     </div>
   );
 }
