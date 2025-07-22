9f56b602b01d2f34e840c4bc710968f81f676c60
erna-h33 <erna.halim33@gmail.com>
2025-07-22 15:32:49 +1000

    useReducer completed

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,38 +1,70 @@
+import { produce } from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment";
+const SET_VALLUE_TO_ADD = "change_value_to_add";
+const DECREMENT_COUNT = "decrement";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
 const reducer = (state, action) => {
-  //
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1;
+      return;
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+    case SET_VALLUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+    default:
+      return;
+  }
 };
 
 function CounterPage({ initialCount }) {
   // const { count, setCount } = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
-    count: initialCount,
-    valueToAdd: 0,
-  });
+  const [state, dispatch] = useReducer(
+    produce(reducer, {
+      count: initialCount,
+      valueToAdd: 0,
+    })
+  );
 
   const increment = () => {
-    // setCount(count + 1);
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
   };
 
   const decrement = () => {
-    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
 
-    // setValueToAdd(value);
+    dispatch({
+      type: SET_VALLUE_TO_ADD,
+      payload: value,
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // setCount(count + valueToAdd);
-    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   return (
